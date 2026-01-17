27b21de52eb43f7b6c7dbdb3ee8ad2849367d474
shuneo <solshuneo@gmail.com>
2026-01-17 18:13:01 +0700

    update

---
+++ loop/main.go
@@ -0,0 +1,25 @@
+package main
+
+import "fmt"
+
+func main() {
+	// standard for loop
+	for i := 1; i < 10; i++ {
+		fmt.Println(i)
+	}
+	// for while
+	// var x = 10
+	// for x != 0 {
+	// 	x -= 1
+	// }
+	// // infinite loop
+	// for {
+	// 	fmt.Println("infinite loop")
+	// 	break
+	// }
+	// // for range
+	// var a = [5]int{1, 2, 3, 4, 5}
+	// for index, value := range a {
+	// 	fmt.Printf("Vị trí %d là giá trị %d\n", index, value)
+	// }
+}
